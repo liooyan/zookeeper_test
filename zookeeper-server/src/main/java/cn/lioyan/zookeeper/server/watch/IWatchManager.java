0a3ec3750586e2634ad6d685aa0445623f01752c
李延 <liyan@wondersoft.cn>
2023-02-21 14:00:39 +0800

    开始 IWatchManager

---
+++ zookeeper-server/src/main/java/cn/lioyan/zookeeper/server/watch/IWatchManager.java
@@ -0,0 +1,16 @@
+package cn.lioyan.zookeeper.server.watch;
+
+/**
+ * {@link org.apache.zookeeper.server.watch.IWatchManager}
+ *<pre>
+ *     监听管理器
+ *
+ *
+ *
+ *</pre>
+ * @author com.lioyan
+ * @date 2023/2/21  13:52
+ */
+public class IWatchManager
+{
+}
