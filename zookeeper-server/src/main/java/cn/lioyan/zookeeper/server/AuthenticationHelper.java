70b079f52f6e33764f563884131bfed8497b12c9
李延 <liyan@wondersoft.cn>
2023-02-21 11:34:18 +0800

    解析 AuthenticationHelper 用户认证

---
+++ zookeeper-server/src/main/java/cn/lioyan/zookeeper/server/AuthenticationHelper.java
@@ -0,0 +1,18 @@
+package cn.lioyan.zookeeper.server;
+
+import org.apache.zookeeper.server.ServerCnxn;
+
+
+/**
+ * {@link org.apache.zookeeper.server.AuthenticationHelper}
+ *<pre>
+ *     用户认证
+ *     {@link org.apache.zookeeper.server.AuthenticationHelper#enforceAuthentication(ServerCnxn, int)}认证用户
+ *
+ *</pre>
+ * @author com.lioyan
+ * @date 2023/2/21  11:32
+ */
+public class AuthenticationHelper
+{
+}
---
+++ zookeeper-server/src/main/java/cn/lioyan/zookeeper/server/auth/AuthenticationProvider.java
@@ -0,0 +1,14 @@
+package cn.lioyan.zookeeper.server.auth;
+
+/**
+ * {@link org.apache.zookeeper.server.auth.AuthenticationProvider}
+ *<pre>
+ *     自定义认证接口
+ *
+ *</pre>
+ * @author com.lioyan
+ * @date 2023/2/21  11:29
+ */
+public interface AuthenticationProvider
+{
+}
